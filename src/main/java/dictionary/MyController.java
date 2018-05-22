7d7a365b01203032e7d1d2e8037716cf1d95425c
Niksior <heroes.VS.misio@gmail.com>
2018-05-22 11:06:33 +0200

    html works

--- src/main/java/dictionary/MyController.java
+++ src/main/java/dictionary/MyController.java
@@ -1,13 +1,12 @@
 package dictionary;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class MyController {
 
-    @RequestMapping(value = "/")
+    @RequestMapping(value = "/custom")
     public String custom() {
         return "index";
     }
