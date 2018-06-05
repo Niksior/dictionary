b6572e67531bbc47477ebdb4c56defc2ee55936d
Niksior <heroes.VS.misio@gmail.com>
2018-06-05 11:37:43 +0200

    ^^

--- src/main/java/dictionary/MyController.java
+++ src/main/java/dictionary/MyController.java
@@ -1,11 +1,23 @@
 package dictionary;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.sql.ResultSet;
+
 @RestController
 public class MyController {
+
+    private final
+    JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public MyController(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
 
     @RequestMapping(value = "/custom")
     public String custom() {
@@ -15,5 +27,24 @@ public class MyController {
     @RequestMapping(value = "/test", produces = "text/plain")
     public String test(@RequestParam("id") String id) {
         return "your id = " + id;
+    }
+
+    @RequestMapping(value = "/addBook", produces = "text/plain")
+    public String tester(@RequestParam("title") String title, @RequestParam("author") String author, @RequestParam("year") String year) {
+        try {
+            String myQuery = "insert into books (author, title, printyear) VALUES (" + author + "," + title + "," + year + ")";
+            jdbcTemplate.execute(myQuery);
+            return "success";
+        } catch (Exception e) {
+            return e.toString();
+        }
+    }
+
+    @RequestMapping(value = "/login", produces = "text/plain")
+    public String login(@RequestParam("name") String name, @RequestParam("passw") String passwd) {
+        try{
+            ResultSet rs
+        }
+        return "";
     }
 }
--- src/main/resources/static/js.js
+++ src/main/resources/static/js.js
@@ -1,6 +1,7 @@
 function addBook() {
     var bookTitle = document.getElementById('addBookName').value;
     var bookAuthor = document.getElementById('addBookAuthor').value;
-    var data = "http://localhost:8181/test?id=" + bookTitle + " " + bookAuthor;
+    var bookYear = document.getElementById('addBookYear').value;
+    var data = "http://localhost:8181/addBook?title='" + bookTitle + "'&author='" + bookAuthor + "'&year=" + bookYear ;
     document.getElementById('response').innerHTML = '<object type="text/plain" data="' + data + '"></object>';
 }
